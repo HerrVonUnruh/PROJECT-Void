6864f2a9594e7ac039c976f12eddf88c799b94b0
Robin Kruck <Robin.Kruck93@web.de>
2022-12-06 13:08:33 +0100

    PILZE!!!

---
+++ Assets/Scripts/ColorSkills/Grün/GrünSprung an&aus/GruenerSkill.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GruenerSkill : MonoBehaviour
+{
+    private PolygonCollider2D GreenColliderSkill;
+    public float bounce = 20f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+        }
+    }
+
+    void Start()
+    {
+        GreenColliderSkill = GetComponent<PolygonCollider2D>();
+
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) || Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            GreenColliderSkill.enabled = !GreenColliderSkill.enabled;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q) || Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            GreenColliderSkill.enabled = false;
+        }
+    }
+}
