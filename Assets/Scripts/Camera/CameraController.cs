c2156314dbd497c8b6e8c0a1899c9bdbb4e33fa7
Rashid <rourogandi@gmail.com>
2022-12-07 11:22:22 +0100

    Rashid camera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject Player;
-
-
+    public GameObject player;
+    public float offset;
+    public float offsetSmoothing;
+    private Vector3 playerPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,17 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z);
+        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
+        if (player.transform.localScale.x > 0f)
+        {
+            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+        }
+        else
+        {
+            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
     }
 }
