bc65abe245b1143dd2b3191015865afc6edfc203
Robin Kruck <Robin.Kruck93@web.de>
2022-12-07 12:03:52 +0100

    Respwn/Dash/MainMenu/PauseMenu/Camera

---
+++ Assets/PauseMenue.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenue : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenueUI;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        PauseMenueUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        PauseMenueUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenueUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
